a4aafa22ccec846830df321fba45bfbd777724fa
Alexey Kolyanov <alexey.kolyanov@gmail.com>
2015-10-29 23:35:42 +0300

    ui changes 3

--- manage.sh
+++ manage.sh
@@ -10,8 +10,14 @@ CONF_MAIN=/opt/gunicorn_main.conf.py
 
 WORK_DIR_API=/opt/creativesuburbs_api
 PYTHON_API=$WORK_DIR_API/env/bin/python
+CELERY_API=$WORK_DIR_API/env/bin/celery
 GUNICORN_API=$WORK_DIR_API/env/bin/gunicorn
 CONF_API=/opt/gunicorn_api.conf.py
+
+WORK_DIR_PLANBOX=/opt/planbox
+PYTHON_PLANBOX=$WORK_DIR_PLANBOX/env/bin/python
+GUNICORN_PLANBOX=$WORK_DIR_PLANBOX/env/bin/gunicorn
+CONF_PLANBOX=/opt/gunicorn_planbox.conf.py
 
 # init sudo
 sudo echo "" > /dev/null
@@ -28,20 +34,22 @@ case $cmd in
             echo "gunicorn seems to be already running"
             exit 133
         fi
-        sudo -u $USER sh -c "cd $WORK_DIR_MAIN/ && $PYTHON_MAIN $WORK_DIR_MAIN/src/manage.py collectstatic --noinput"
+        sudo -u $USER sh -c "cd $WORK_DIR_MAIN/ && grunt && $PYTHON_MAIN $WORK_DIR_MAIN/src/manage.py collectstatic --noinput"
         sudo -u $USER sh -c "cd $WORK_DIR_API/ && $PYTHON_API $WORK_DIR_API/src/manage.py collectstatic --noinput"
+        sudo -u $USER sh -c "cd $WORK_DIR_PLANBOX/ && $PYTHON_PLANBOX $WORK_DIR_PLANBOX/src/manage.py collectstatic --noinput"
         DAEMONFLAG="-D"
         STARTMSG="daemon"
         [ "$param1" = "--debug" ] && DAEMONFLAG="" && STARTMSG="interactive"
         sudo -u $USER sh -c "cd $WORK_DIR_MAIN/src; $PYTHON_MAIN -W ignore $GUNICORN_MAIN -c $CONF_MAIN --chdir $WORK_DIR_MAIN/src project.wsgi:application $DAEMONFLAG"
         sudo -u $USER sh -c "cd $WORK_DIR_API/src; $PYTHON_API -W ignore $GUNICORN_API -c $CONF_API --chdir $WORK_DIR_API/src project.wsgi:application $DAEMONFLAG"
+        sudo -u $USER sh -c "cd $WORK_DIR_PLANBOX/src; $PYTHON_PLANBOX -W ignore $GUNICORN_PLANBOX -c $CONF_PLANBOX --chdir $WORK_DIR_PLANBOX/src planbox.wsgi:application $DAEMONFLAG"
         if [ $? -ne 0 ]; then
             echo "ERROR starting gunicorn"
             exit 127
         else
-            echo "Started $STARTMSG"
+            echo "Started gunicorn $STARTMSG"
         fi
-        sudo -u $USER sh -c "cd $WORK_DIR_API/src; $PYTHON_API $WORK_DIR_API/src/manage.py celery worker -D --concurrency=2 --logfile=/var/log/creativesuburbs/celery.log --pidfile=/tmp/celery.pid"
+        sudo -u $USER sh -c "cd $WORK_DIR_API/src; $CELERY_API worker -A project.celery -D --concurrency=2 --logfile=/var/log/creativesuburbs/celery.log --pidfile=/tmp/celery.pid"
         sudo service nginx restart
         ;;
     stop)
